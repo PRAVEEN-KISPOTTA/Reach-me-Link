815f96f11c45d08726b732ead2095de4e089b2fe
Praveen Kispotta <90713768+PRAVEEN-KISPOTTA@users.noreply.github.com>
2023-01-17 01:10:09 +0530

    Initial commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Use props to add styles, link and images so that it looks like the reference image.
+
+class ListItem extends React.Component {
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="ListItem" style={{ backgroundColor: item.bgColor }}>
+        <a href={item.link}>
+          <img src={item.icon} alt={item.name} />
+        </a>
+      </div>
+    );
+  }
+}
+
+export default ListItem;
